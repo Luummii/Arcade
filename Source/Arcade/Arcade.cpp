dc130a9766f911e3c0be8d4970b8255edeaa0e8e
Andrey Zimin Luummii <luummii@vuejs.fun>
2018-06-04 11:17:24 +0300

    Add c_cpp_properties.json settings

--- Source/Arcade/Arcade.cpp
+++ Source/Arcade/Arcade.cpp
@@ -1,6 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Arcade.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arcade, "Arcade" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Arcade, "Arcade");
